08c0d3d43a9f6545179d849637cec0ce460a38f4
prabhats <nimishasinha843@gmail.com>
2026-06-10 14:54:12 +0530

    Added AzureRM backend block

--- provider.tf
+++ provider.tf
@@ -1,4 +1,6 @@
 terraform {
+  backend "azurerm" {}
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -6,9 +8,9 @@ terraform {
     }
   }
 }
+
 provider "azurerm" {
   features {}
   subscription_id = "582e7b86-d28f-4458-b727-2305c8849192"
-
 }
 
